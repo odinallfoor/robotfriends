779d91defae737981081e4152bd1e25de46b4084
Carlos Garrido <cgarrido@technisys.com>
2020-01-20 16:40:38 -0300

    Initial Commit

---
+++ build/precache-manifest.adb6d45187ef1353910a1ec605aab303.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a479ce08dc2a24d97a83e6b59f7ca4f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "86b8146436c21d3551d8",
+    "url": "/static/css/main.39e2ce6c.chunk.css"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/static/js/2.37296ba2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.37296ba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86b8146436c21d3551d8",
+    "url": "/static/js/main.e3701b33.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/containers/App.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+import {robots} from "../robots";
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots : robots,
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users');
+        this.setState({robots: robots})
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    };
+
+    render() {
+        const {robots, searchField} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+        return !robots.length ?
+        <h1>Loading...</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
+    }
+}
+
+export default App;
